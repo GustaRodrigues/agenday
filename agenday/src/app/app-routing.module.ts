fcace5b8587b4a8172e3b4b3946006e798896d83
Rodrigo Matsumoto <rodrigo.matsumoto87@gmail.com>
2021-04-23 00:26:45 -0300

    'Merge' da branch feature/login (resolvendo conflitos)

--- agenday/src/app/app-routing.module.ts
+++ agenday/src/app/app-routing.module.ts
@@ -8,9 +8,14 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+
 ];
 
 @NgModule({
