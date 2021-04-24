1c3f1a6c640a55990e6b908c3165e590eab69230
Flavio Germano <germanoperez2000@gmail.com>
2021-04-23 23:42:26 -0300

    Criaçao da pagina de registro

--- agenday/src/app/app-routing.module.ts
+++ agenday/src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
 
 ];
 
